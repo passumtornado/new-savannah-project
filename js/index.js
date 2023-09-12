50ece94a6370197c38724e7b0ede1b8d590893c5
passumtornado <passumtornado10@gmail.com>
2023-09-12 10:55:17 +0000

    new changes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ let Hamburger = document.querySelector('#hamburgerIcon');
 let menu = document.querySelector('.nav-menu');
 let sideRightLink = document.querySelectorAll('.sidebar-right-links')
 let accordionBtn = document.querySelectorAll('.accordion-btn-section')
+let accordionPanel = document.querySelector('.accordion-panel')
+let middleCardBtn = document.querySelectorAll(".first-card-buttons .btn")
+
+console.log(accordionPanel )
 menu.addEventListener('click',()=>{
     menu.classList.toggle('active');
 })
@@ -15,30 +19,42 @@ menu.addEventListener('click',()=>{
     })
  })
 
+middleCardBtn.forEach(btn=>{
+  btn.addEventListener('click',()=>{
+    middleCardBtn.forEach(btn=>{
+      btn.classList.remove('active')
+    });
+    btn.classList.add('active')
+  })
+})
 
  accordionBtn.forEach(accordion =>{
- 
     accordion.addEventListener('click',()=>{
-        this.classList.toggle('active');
-    var panel = this.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-      if (accordionTitle) {
-        accordionTitle.style.marginBottom = '0';
-        accordionBtnOpen.classList.remove('active');
-        accordionBtnClose.classList.remove('active');
-        accordionSection.classList.remove('active');
-        
+      accordionBtn.forEach(accordion=>{
+        accordion.classList.remove('active')
+      })
+      accordion.classList.add('active')
+      let accordionPanel = accordion.nextElementSibling
+      console.log(accordionPanel)
+      if (accordionPanel.style.maxHeight) {
+        accordionPanel.style.maxHeight = null;
+        accordion.classList.remove('active')
+        accordionPanel.classList.remove('active')
+        if (accordion) {
+          accordion.style.marginBottom = '0';
+          // accordionBtnOpen.classList.remove('active');
+          // accordionBtnClose.classList.remove('active');
+          // accordionSection.classList.remove('active');
+          
+        }
+  
+      } else {
+         accordionPanel.style.maxHeight = `${accordionPanel.scrollHeight}px`
+         accordionPanel.classList.add('active')
+        // accordionBtnOpen.addClass = 'active';
+        // accordionBtnClose.addClass = 'active';
+        // accordionSection.classList.add('active');
+  
       }
-
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
-      if (accordionTitle) accordionTitle.style.marginBottom = '25px';
-      accordionBtnOpen.addClass = 'active';
-      accordionBtnClose.addClass = 'active';
-      accordionSection.classList.add('active');
-
-    }
     })
  })
